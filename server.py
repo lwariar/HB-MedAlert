006a0ab96836fe4fc9d3c7fd3178d5ae076b8da5
lwariar <lakshmiwariar@hotmail.com>
2020-11-11 22:08:54 +0000

    Changes to the search funtion

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-""" APIs,Flask routes """
+""" Flask routes """
 
 import requests
 import os
@@ -9,7 +9,7 @@ import crud
 from model import connect_to_db
 
 
-API_KEY = os.environ['OPENFDA_API_KEY']
+#API_KEY = os.environ['OPENFDA_API_KEY']
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -18,14 +18,108 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """View login"""
-    
     return render_template("login.html")
+
+@app.route('/signin')
+def signin():
+    """View login page"""
+    return render_template("login.html")
+
+@app.route('/login', methods=['POST'])
+def user_login():
+    """Log a user into the website"""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.check_user_login_info(email, password)
+
+    if user:
+        #add user id to the session
+        session["user_id"] = user.user_id
+        device = crud.get_devices_by_user_id(user.user_id)
+        drug = crud.get_drugs_by_user_id(user.user_id)
+        return render_template("search.html", user=user, device=device, drug=drug)
+    else:
+        flash("Login info incorrect, please try again")
+        return redirect('/signin')
 
 @app.route('/about')
 def about():
     """View about"""
-    
     return render_template("about.html")
+
+@app.route('/add-user')
+def add_user():
+    """View add user page"""
+    return render_template("register.html")
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    """add a user info to users table"""
+    
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    tel_num = request.form.get('tel_num')
+    caregiver_email = request.form.get('caregiver_email')
+    
+    user = crud.get_user_by_email(email)
+    
+    """Check to see if user is already in database"""
+    if user:
+        flash("This email already exists. Please try again")
+        return redirect('/')
+    else:
+        user = crud.create_user(email, password, fname, lname, tel_num, caregiver_email)
+
+        #add user id to the session
+        session["user_id"] = user.user_id
+        flash("Your account was created successfully")
+        
+        return render_template("login.html")
+
+@app.route('/addupdate')
+def add_update():
+    """View add-update """
+
+    user_id = session.get("user_id")
+
+    if user_id:
+        user = crud.get_user_by_id(user_id)
+        device = crud.get_devices_by_user_id(user.user_id)
+        drug = crud.get_drugs_by_user_id(user.user_id)
+        return render_template("addupdate.html", user=user, device=device, drug=drug)
+    else:
+        flash("Please sign in")
+        return render_template("login.html")
+
+        """
+        #add drug/device for the user
+        qtype = request.form.get('qtype')
+        name = request.form.get('name')
+        mname = request.form.get('mname')
+
+        if qtype == 'device':
+            # model_num = request.form.get('model_num') // we're not using this now
+            # serial_num = request.form.get('serial_num') // we're not using this now
+            device = crud.add_device(name, "", "", mname, user.user_id)
+        elif qtype == "drug':":
+            drug = crud.add_drug(name, mname, user.user_id)
+        device = crud.get_devices_by_user_id(user.user_id)
+        drug = crud.get_drugs_by_user_id(user.user_id)
+
+        #add drug/device for the user
+        qtype = request.form.get('qtype')
+        name = request.form.get('name')
+        mname = request.form.get('mname')
+
+        if qtype == 'device':
+            device = crud.add_device(name, "", "", mname, user.user_id)
+        elif qtype == "drug':":
+            drug = crud.add_drug(name, mname, user.user_id)
+        """
 
 @app.route('/search')
 def search():
@@ -49,84 +143,10 @@ def profile():
 
     if user_id:
         user = crud.get_user_by_id(user_id)
-        device = crud.get_devices_by_user_id(user.user_id)
-        drug = crud.get_drugs_by_user_id(user.user_id)
-        return render_template("profile.html", user=user, device=device, drug=drug)
+        return render_template("profile.html", user=user)
     else:
         flash("Please sign in")
         return render_template("login.html")
-
-
-@app.route('/signin')
-def signin():
-    """View login page"""
-    
-    return render_template("login.html")
-
-@app.route('/add-user')
-def add_user():
-    """View add user page"""
-    
-    return render_template("register.html")
-
-@app.route('/register', methods=['POST'])
-def register_user():
-    """add a user info to users table"""
-    
-    fname = request.form.get('fname')
-    lname = request.form.get('lname')
-    email = request.form.get('email')
-    password = request.form.get('password')
-    tel_num = request.form.get('tel_num')
-    caregiver_email = request.form.get('caregiver_email')
-    
-    user = crud.get_user_by_email(email)
-    
-    """Check to see if user is already in database"""
-    if user:
-        flash("This email already exists. Please try again")
-        return redirect('/')
-    else:
-        user = crud.create_user(email, password, fname, lname, tel_num, caregiver_email)
-
-        #add drug/device for the user
-        qtype = request.form.get('qtype')
-        name = request.form.get('name')
-        mname = request.form.get('mname')
-
-        if qtype == 'device':
-            # model_num = request.form.get('model_num') // we're not using this now
-            # serial_num = request.form.get('serial_num')
-            device = crud.add_device(name, "", "", mname, user.user_id)
-        elif qtype == "drug':":
-            drug = crud.add_drug(name, mname, user.user_id)
-        #add user id to the session
-        session["user_id"] = user.user_id
-        flash("Your account was created successfully")
-    
-        device = crud.get_devices_by_user_id(user.user_id)
-        drug = crud.get_drugs_by_user_id(user.user_id)
-        return render_template("search.html", user=user, device=device, drug=drug)
-
-@app.route('/login', methods=['POST'])
-def user_login():
-    """Log a user into the website"""
-
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    user = crud.check_user_login_info(email, password)
-
-    if user:
-        #add user id to the session
-        if "user_id" not in session:
-            session["user_id"] = user.user_id
-        device = crud.get_devices_by_user_id(user.user_id)
-        drug = crud.get_drugs_by_user_id(user.user_id)
-        return render_template("search.html", user=user, device=device, drug=drug)
-    else:
-        flash("Login info incorrect, please try again")
-        return redirect('/signin')
 
 @app.route('/update-profile', methods=['POST'])
 def update_profile():
@@ -142,15 +162,6 @@ def update_profile():
     user_id = session.get("user_id")
     user = crud.update_user(user_id, fname, lname, email, password, tel_num, caregiver_email)
     if user:
-        #add drug/device for the user
-        qtype = request.form.get('qtype')
-        name = request.form.get('name')
-        mname = request.form.get('mname')
-
-        if qtype == 'device':
-            device = crud.add_device(name, "", "", mname, user.user_id)
-        elif qtype == "drug':":
-            drug = crud.add_drug(name, mname, user.user_id)
         flash("Update successful")
     else:
         flash("Oops! Something went wrong!")
@@ -158,32 +169,6 @@ def update_profile():
     device = crud.get_devices_by_user_id(user.user_id)
     drug = crud.get_drugs_by_user_id(user.user_id)
     return render_template("search.html", user=user, device=device, drug=drug)
-
-# @app.route('/query', methods=['POST'])
-# def query():
-#     """build a query based on user input"""
-
-#     qtype = request.form.get('qtype')
-#     search_term = request.form.get('search_term')
-#     print(qtype, search_term)
-
-#     URL = f'https://api.fda.gov/{qtype}/enforcement.json?search=product_description:RANITIDINE&limit=10'
-
-#     data = requests.get(URL).json()
-
-#     for result in data.get('results', []):
-#         print(result['product_description'])
-#         print(result['reason_for_recall'])
-#         print('*****************************************************************************************************')
-
-#     return redirect('/')
-# """
-# """
-# URL = 'https://api.fda.gov/drug/event.json?search=patient.reaction.reactionmeddrapt:"fatigue"&limit=5'
-
-# 'https://api.fda.gov/device/event.json?search=device.generic_name:glucometer&limit=1'
-
-# URL = 'https://api.fda.gov/drug/enforcement.json?search=product_description:RANITIDINE&limit=10'
 
 if __name__ == '__main__':
     connect_to_db(app)
--- static/js/api_query_display.js
+++ static/js/api_query_display.js
@@ -1,38 +1,70 @@
 'use strict';
 
+//global results var
+var queryResults;
+
 function apiQueryResults(evt) 
 {
     evt.preventDefault();
 
     // get the users selection
     var qstr = $('#ddlist :selected').text().split("~");
+
+    $( "#results" ).empty();
     // build the query
-    URL = 'https://api.fda.gov/' + qstr[0] + '/enforcement.json?search=product_description:'+ qstr[1] + '&limit=1';
+    if (qstr == "All")
+    {
+        alert('All');
+    }
+    else
+        URL = 'https://api.fda.gov/' + qstr[0] + '/enforcement.json?search=product_description:'+ qstr[1] + '&limit=1';
     
-    //var email_body;
-
+    
     $.get(URL, function( data ) {
+        //if results are found
+        if (data)
+        {
+            queryResults = data;
+            $('#results').append(`<br>Product Description: ${data.results[0]['product_description']}`);
+            $('#results').append(`<br>Reason for recall:<i><b> ${data.results[0]['reason_for_recall']} </b></i>`);
+            $('#results').append(`<br>Classification: ${data.results[0]['classification']}`);
         
-        $('#results').append(`<br>Product Description: ${data.results[0]['product_description']}`);
-        $('#results').append(`<br>Reason for recall:<i><b> ${data.results[0]['reason_for_recall']} </b></i>`);
-        $('#results').append(`<br>Classification: ${data.results[0]['classification']}`);
-        
-        //display dates in the mm/dd/yyyy format
-        var r_date = data.results[0]['recall_initiation_date'];
-        r_date = r_date.substring(4,6) +'/' + r_date.substring(6,8) + '/' + r_date.substring(0,4);
-        $('#results').append(`<br>Recall Initiation Date: ${r_date}`);
-        var t_date = data.results[0]['termination_date'];
-        t_date = t_date.substring(4,6) +'/' + t_date.substring(6,8) + '/' + t_date.substring(0,4);
+            //display dates in the mm/dd/yyyy format
+            var r_date = data.results[0]['recall_initiation_date'];
+            r_date = r_date.substring(4,6) +'/' + r_date.substring(6,8) + '/' + r_date.substring(0,4);
+            $('#results').append(`<br>Recall Initiation Date: ${r_date}`);
+            
+            var t_date = data.results[0]['termination_date'];
+            //check if there is a termination date
+            if (t_date)
+                t_date = t_date.substring(4,6) +'/' + t_date.substring(6,8) + '/' + t_date.substring(0,4);
+            else
+                t_date = "";
+            $('#results').append(`<br>Termination Date: ${t_date}`);
+            $('#results').append(`<br>Voluntary Mandated: ${data.results[0]['voluntary_mandated']}`);
+            $('#results').append(`<br>Status: ${data.results[0]['status']}`);
 
-        $('#results').append(`<br>Termination Date: ${t_date}`);
-        $('#results').append(`<br>Voluntary Mandated: ${data.results[0]['voluntary_mandated']}`);
-        $('#results').append(`<br>Status: ${data.results[0]['status']}`);
-
-        //email_body = "Product Description: " + data.results[0]['product_description'] +
-        //        "Reason for recall: " + data.results[0]['reason_for_recall'];
+            //enable the email button
+            $('#emailthis').prop('disabled', false);
+        }
+        else
+        {
+            $('#results').append(`<br> No results found.`);
+            //enable the email button
+            $('#emailthis').prop('disabled', true);
+        }
     });
+}   
+$('#search').on('click', apiQueryResults);
 
-    //let subject = "From MedAlert!";    
-    //let mailurl = "mailto:lakshmiwariar@hotmail.com,?subject=" + subject + "&body="  + email_body;
-    //window.open(mailurl);
+function sendEmail(evt) 
+{
+    evt.preventDefault();
+    //get the text from #results on the page
+    var email_body = $('#results').text();
+    let user_email = $('#useremail').text();
+    let subject = "From MedAlert!";
+    let mailurl = "mailto:" + user_email + ",?subject=" + subject + "&body="  + email_body;
+    window.open(mailurl);
 }
+$('#emailthis').on('click', sendEmail);
